70a98e8f317709b6e7cd7846774924d07c195ebf
arve0 <arve.seljebu@gmail.com>
2014-12-07 01:56:36 +0100

    use sync

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var basename  = require('path').basename;
-var debug     = require('debug')('metalsmith-pandoc');
 var dirname   = require('path').dirname;
 var extname   = require('path').extname;
-var pandoc    = require('pandoc-bin').path;
+var debug     = require('debug')('metalsmith-pandoc');
+var pdcPath   = require('pandoc-bin').path;
 var pdc       = require('pdc');
 var minimatch = require('minimatch');
+var Sync      = require('sync');
 
 
 // use pandoc-bin
-pdc.path = pandoc;
+pdc.path = pdcPath;
 
 /**
  * Expose `plugin`.
@@ -30,22 +31,24 @@ function plugin(options){
   var pattern = options.pattern || '**/*.md';
 
   return function(files, metalsmith, done){
-    Object.keys(files).forEach(function(file){
-      debug('checking file: %s', file);
-      if (!minimatch(file, pattern)) return;
-      var data = files[file];
-      var dir = dirname(file);
-      var html = basename(file, extname(file)) + '.html';
-      if ('.' != dir) html = dir + '/' + html;
+    Sync(function(){
+      Object.keys(files).forEach(function(file){
+        debug('checking file: %s', file);
+        if (!minimatch(file, pattern)) return;
+        var data = files[file];
+        var dir = dirname(file);
+        var html = basename(file, extname(file)) + '.html';
+        if ('.' != dir) html = dir + '/' + html;
 
-      debug('converting file: %s', file);
-      pdc(data.contents.toString(), from, to, args, opts, function(err, str){
-        debug('converted %s: %s', file, str.substring(0,20));
-        if(err) return;
+        debug('converting file: %s', file);
+        var str = pdc.sync(null, data.contents.toString(), from, to, args, opts);
+        debug('converted %s: %s...', file, str.substring(0,25));
         data.contents = new Buffer(str);
         delete files[file];
         files[html] = data;
       });
-    });
+      debug('done');
+      done();
+    })
   };
 }
